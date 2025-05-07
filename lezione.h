65cebed6fe9497373914b111f2296f39c12308d9
Michele Vitiello <michelemicky2005@gmail.com>
2025-05-07 14:22:24 +0200

    Eliminata la cartella .vscode

--- lezione.h
+++ lezione.h
@@ -7,4 +7,4 @@ typedef struct lezione *Lezione;
 Lezione crea_Lezione(int id, const char* nome, Data dat, const char* orario, int posti_max);
 void libera_lezione(Lezione l); //dealloca una lezione dalla memoria quando non serve piu'
 void visualizza_lezione(Lezione l);
-int prenota_lezioe(Data oggi, Cliente c); //ritorna 1 se e' stata prenotata, 0 altrimenti
+int prenota_lezione(Data oggi, Cliente c); //ritorna 1 se e' stata prenotata, 0 altrimenti
